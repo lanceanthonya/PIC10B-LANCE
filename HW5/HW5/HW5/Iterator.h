2f2500614da5bd1da80a3d2ff2e085cb0b77c246
lanceanthonya <97372002+lanceanthonya@users.noreply.github.com>
2022-05-30 16:23:00 -0700

    Iterator.h written
    Wrote Iterator.h based on the slides

--- HW5/HW5/HW5/Iterator.h
+++ HW5/HW5/HW5/Iterator.h
@@ -19,18 +19,24 @@ class Iterator
 public:
     Iterator();
     std::string get() const;  // Use operator* instead
-    void next();         // Use ++ instead
-    void previous(); // Use -- instead
-    bool equals(Iterator b) const; //Use == instead
+    
+    std::string operator*();
+    
+    void operator++();
+    
+    void operator--();
+    
+    bool operator==(Iterator second) const;
+
+
     
 private:
     
     friend class List;
     Node* position;
-    const List* bug;
-  
-    
+    const List* container;
 };
+
 
 
 #endif /* __ITERATOR__ */
